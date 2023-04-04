c1056c387571373299fae0ae539cdd23f30aa8c4
Drew Pirrone-Brusse <dpirrone-brusse@kcftech.com>
2023-04-04 17:08:54 -0400

    Add some *nix stuff

---
+++ linux/wikioify.sh
@@ -0,0 +1,24 @@
+# Use pandoc to convert markdown into HTML, with a <details> footer explaining
+# how to use pandoc to convert markdown into HTML.
+function wikioify {
+cat << EOF
+$(pandoc ${1} -t html)
+
+<hr>
+
+<details>
+  <summary>Original Markdown</summary>
+  Generated with
+  <pre><code>pandoc \${1} -t html
+
+&lt;hr&gt;
+
+&lt;details&gt;
+  &lt;summary&gt;Original Markdown&lt;/summary&gt;
+  Generated with
+  &lt;pre&gt;&lt;code&gt;\$(cat \${1} | jq -rR &apos;@html&apos;)&lt;/code>&lt;/pre&gt;
+&lt;/details&gt;</pre></code>
+  <pre><code>$(cat ${1} | jq -rR '@html')</code></pre>
+</details>
+EOF
+}
