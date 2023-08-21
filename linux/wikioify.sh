fbb7848516057ed5eee0d3e16e0647df8991c132
Drew Pirrone-Brusse <drew.pirrone.brusse@gmail.com>
2023-08-21 11:23:08 -0400

    Cleanup wikioify.sh

--- linux/wikioify.sh
+++ linux/wikioify.sh
@@ -1,5 +1,6 @@
 # Use pandoc to convert markdown into HTML, with a <details> footer explaining
 # how to use pandoc to convert markdown into HTML.
+# TODO: Error when `pandoc` or `jq` are not installed
 function wikioify {
 cat << EOF
 $(pandoc ${1} -t html)
